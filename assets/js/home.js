a06388223188be2e8d6a2efd4e8b48d0100f11f7
RoirrawW <lucas.hatron@gmail.com>
2022-09-20 14:52:54 +0200

    collect7

--- assets/js/home.js
+++ assets/js/home.js
@@ -1,2 +1,17 @@
-screen.orientation.lock(); 
-screen.lockOrientation('landscape');
+let modal = document.getElementById("myModal");
+let btn = document.getElementById("register");
+let span = document.getElementsByClassName("close")[0];
+
+btn.onclick = function() {
+  modal.style.display = "block";
+}
+
+span.onclick = function() {
+  modal.style.display = "none";
+}
+
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+}
