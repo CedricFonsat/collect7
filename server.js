fd772f6c33e14cede7dd14471bfd8ffcd926d909
cedricfonsat <79462084+CedricFonsat@users.noreply.github.com>
2022-09-20 10:20:05 +0200

    gfhgvjhbj

--- server.js
+++ server.js
@@ -1,14 +1,17 @@
 import express from 'express';
+import mongoose from "mongoose";
 import userRouter from './routes/userRouter.js'
 import adminRouter from './routes/adminRouter.js'
 import visitorRouter from './routes/visitorRouter.js'
 import cors from 'cors'
+import session from 'express-session';
 import 'dotenv/config'
 
-
+const db = process.env.BDD_URL
 const app = express()
 const router = express.Router()
 
+app.use(session({secret: process.env.SECRET_KEY,saveUninitialized: true, resave: true}))
 app.use(cors())
 app.use(express.static('./assets'))
 app.use(express.urlencoded({extended: true}))
@@ -30,3 +33,13 @@ app.listen(process.env.PORT, function(err){
         console.log(`connected to ${process.env.APP_URL}`);
     }
 })
+
+mongoose.connect(db,(err)=>{
+    if (err) {
+        console.log(err);
+    }else{
+        console.log("connected to database mongodb (c'est dur....)");
+    }
+})
+
+export default router
